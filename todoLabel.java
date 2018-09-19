80352d8158eb64ed10f6f7b25f494ae1b63bafde
Oliver Eriksson <olieri@kth.se>
2018-09-19 13:38:29 +0200

    initial commit

---
+++ todoLabel.java
@@ -0,0 +1,14 @@
+
+public class todoLabel {
+	private String description;
+	private int duedate;
+	private boolean completed;
+	
+	public todoLabel(String description, int duedate){
+		this.description = description;
+		this.duedate = duedate;
+		this.completed = false;
+	}
+	
+	
+}
