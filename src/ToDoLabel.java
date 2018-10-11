ca46b8e34dd3ddc8ac8d66525352bd0d7cfb0468
Olivers mac <olieri@kth.se>
2018-10-12 00:01:37 +0200

    Implement add, swap, sort and set prioritize-order of labels in todolist.

--- src/ToDoLabel.java
+++ src/ToDoLabel.java
@@ -7,15 +7,17 @@ import java.util.Date;
  * The todoLabel contains data about an issue the user might want to solve.
  * The user is able to describe the issue, set a due date for it to be completed
  */
-public class ToDoLabel {
+public class ToDoLabel implements Comparable<ToDoLabel>{
     private String headtext;
     private ArrayList<String> description;
     private Date duedate;
     private boolean completed;
+    private int priority;
 
-    public ToDoLabel(String headtext, Date duedate, String... description){
+    public ToDoLabel(String headtext, int priority, Date duedate, String... description){
         this.headtext = headtext;
         this.duedate = duedate;
+        this.priority = priority;
         this.completed = false;
         this.description = new ArrayList<>();
         Collections.addAll(this.description, description);
@@ -49,11 +51,36 @@ public class ToDoLabel {
         return completed;
     }
 
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
     /**
      * Toggles the completed status of the label
      * @return true if the label was toggled from false to true
      */
     public boolean toggleCompleted() {
         return completed = !completed;
+    }
+
+    /**
+     * Compares labels by completed and priority to enable sorting
+     * @param o the object to compare
+     * @return
+     */
+    @Override
+    public int compareTo(ToDoLabel o) {
+        if(this.completed && !o.isCompleted()){
+            return -1;
+        }else if(this.completed && o.isCompleted()){
+            return this.duedate.compareTo(o.duedate);
+        }else if((!this.completed && o.isCompleted())) {
+            return 1;
+        }
+        return Integer.compare(this.priority, o.priority);
     }
 }
--- src/ToDoList.java
+++ src/ToDoList.java
@@ -1,7 +1,66 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
-public class ToDoList extends ArrayList<ToDoList>{
+public class ToDoList{
+    private ArrayList<ToDoLabel> labelList;
     public ToDoList(){
-        super();
+        labelList = new ArrayList<>();
+    }
+
+    public ToDoList(ToDoLabel... labels){
+        labelList = new ArrayList<>();
+        Collections.addAll(labelList, labels);
+    }
+
+    public void addLabel(ToDoLabel label){
+        labelList.add(0, label);
+        prioritizeByOrder();
+    }
+
+    public void changeLabelPosition(int from, int to){
+        if(from < 0 || from >= labelList.size()){
+            throw new IndexOutOfBoundsException("Index '"+from+"' is not a valid position index");
+        }else if(to < 0 || to >= labelList.size()){
+            throw new IndexOutOfBoundsException("Index '"+to+"' is not a valid position index");
+        }else if(from == to){
+            return;
+        }
+        ToDoLabel label = labelList.get(from);
+        if(!label.isCompleted()){
+            int c = getFirstCompletedLabel();
+            if(to <= c) {
+                if(from > to) {
+                    label = labelList.remove(from);
+                    labelList.add(to, label);
+                }else{
+                    labelList.add(to, label);
+                    labelList.remove(from);
+                }
+            }else{
+                labelList.add(c, label);
+                labelList.remove(from);
+            }
+        }
+        prioritizeByOrder();
+    }
+
+    private int getFirstCompletedLabel(){
+        sortByPriority();
+        for (int i = 0; i < labelList.size(); i++) {
+            if(labelList.get(i).isCompleted())
+                return i;
+        }
+        return -1;
+    }
+
+    private void prioritizeByOrder(){
+        for (int i = 0; i < labelList.size(); i++) {
+            labelList.get(i).setPriority(i);
+        }
+    }
+
+
+    public void sortByPriority(){
+        Collections.sort(labelList);
     }
 }
