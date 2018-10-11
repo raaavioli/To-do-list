50dd865a48ab29b1d54e5d8bf2cd779316acc0bc
Olivers mac <olieri@kth.se>
2018-10-11 21:23:01 +0200

    complete todoLabel constructor

--- src/todoLabel.java
+++ src/todoLabel.java
@@ -1,14 +1,17 @@
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class todoLabel {
-	private String description;
+	private String headtext;
+	private ArrayList<String> description;
 	private int duedate;
 	private boolean completed;
+	private int priorityIndex;
 	
-	public todoLabel(String description, int duedate){
-		this.description = description;
+	public todoLabel(String headtext, int duedate, String... description){
+		this.headtext = headtext;
 		this.duedate = duedate;
 		this.completed = false;
+		Collections.addAll(this.description, description);
 	}
-	
-	
 }
